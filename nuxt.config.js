d054e0053525c04a6b71ed587c61faa12ee2d923
Snorre Aas <saa@24sevenoffice.com>
2021-08-26 12:59:41 +0200

    build dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  generate: {
-    dir: 'dist'
-  },
+  buildDir: 'dist',
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
