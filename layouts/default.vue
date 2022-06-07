70f8a0931c322e298b4d2bace9bb01e64f6c7b82
Snorre Aas <snorre.a.aas@hotmail.com>
2022-06-07 14:18:19 +0200

    feat: updated body background

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,8 @@
 </template>
 
 <style lang="sass">
+body
+    background: #212121
 *
   font-family: monospace, system-ui,-apple-system,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"
 </style>
