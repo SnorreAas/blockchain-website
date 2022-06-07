46386f09118da31f956a1d71c02e96905b082ccf
Snorre Aas <snorre.a.aas@hotmail.com>
2022-06-07 14:07:37 +0200

    feat: :guitar: (Moved comps to layout): adjustments)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <Navbar title="AI GOBLINS" />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <style lang="sass">
-  *
-    font-family: monospace, system-ui,-apple-system,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"
+*
+  font-family: monospace, system-ui,-apple-system,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"
 </style>
