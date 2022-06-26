10eb6dcbcf41f9e5c621f5dbbc676dc78975a745
Snorre Aas <snorre.a.aas@hotmail.com>
2022-06-26 22:34:32 +0200

    Revert "feat: added google analytics"
    This reverts commit 5228e79b678a212f7175c6f05624623167e7f520.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=G-N6C67XWZZD"
-></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-N6C67XWZZD");
-</script>
-
 <template>
   <div>
     <Nuxt />
