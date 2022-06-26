98b5a12aedbc22895fe5dd4b91f8e649588a9ab5
Snorre Aas <snorre.a.aas@hotmail.com>
2022-06-26 22:24:24 +0200

    gtm

--- plugins/gtm.js
+++ plugins/gtm.js
@@ -5,6 +5,25 @@ export default () => {
   /*
    ** Include Google Tag Manager
    */
+  (function (i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    (i[r] =
+      i[r] ||
+      function () {
+        (i[r].q = i[r].q || []).push(arguments);
+      }),
+      (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(
+    window,
+    document,
+    "script",
+    "https://www.google-analytics.com/analytics.js",
+    "ga"
+  );
 
   (function (w, d, s, l, i) {
     w[l] = w[l] || [];
